3f58829ac10fb29d33c3bcc4e0388f96fa7f4097
Oggi Danailov <ogidan@abv.bg>
2017-02-15 15:21:58 -0500

    average 30 days chart sum done

--- public/js/chartMonth.js
+++ public/js/chartMonth.js
@@ -1,7 +1,9 @@
-function sum(array) { return (array.reduce(function(a,b){return a + b},0))/24 }
+function sum(array) { return (array.reduce(function(a,b){return a + b},0))/24 } //gets the average number of each day
+
 
 $.getJSON('data/history.json', function(dataJSON){
 	var array1 = dataJSON.services[0].values //the pure values of Dashboard 
+	// console.log(array1)
 	var results = [];
       while (array1.length-1) {
         results.push(array1.splice(0, 24)); //new array results has now 30 subarrays
@@ -11,7 +13,24 @@ $.getJSON('data/history.json', function(dataJSON){
         sums.push(results[i])
       }
 
+
       result = sums.map(function(month) {
+            return sum(month)
+       })
+
+    var array2 = dataJSON.services[1].values
+    // console.log(array2)
+
+    var results2 = [];
+    while (array2.length-1) {
+        results2.push(array2.splice(0, 24)); //new array results has now 30 subarrays;one of each day
+      }
+    var sums2 = []
+      for(i=0; i<results2.length; i++) {
+        sums2.push(results2[i])
+      }
+
+      result2 = sums2.map(function(month) {
             return sum(month)
        })
 
@@ -23,6 +42,10 @@ $.getJSON('data/history.json', function(dataJSON){
 
     title: {
         text: 'Status data of Electrical current'
+    },
+
+    subtitle: {
+        text: 'It gives the average sum of each day'
     },
 
     yAxis: {
@@ -48,6 +71,13 @@ $.getJSON('data/history.json', function(dataJSON){
             return sum(month)
        })
     },
+
+    	{
+        name: dataJSON.services[1].name,
+        data: result2 = sums2.map(function(month) {
+            return sum(month)
+       })
+    }
 
     ]
 
